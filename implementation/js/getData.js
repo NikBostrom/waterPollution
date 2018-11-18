e1c9cd89df7b9d869ce189971ed8b0ad60fa2e41
Ballinchanman <cbrown02@college.harvard.edu>
2018-11-17 23:58:02 -0500

    got some water data

---
+++ implementation/js/getData.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    // $("button").click(function(){
+    $.getJSON("https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&stateCd=ca&parameterCd=00065,00300,99133,72240,30208,63680&siteStatus=all", function(result){
+        // console.log(result);
+        // $.each(result, function(i, field){
+        //     $("div").append(field + " ");
+        // });
+        var jsonData = JSON.stringify(result);
+        download(jsonData, 'waterdata.json', 'json');
+        console.log("done");
+    });
+    // });
+});
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+// download(jsonData, 'json.txt', 'text/plain');
